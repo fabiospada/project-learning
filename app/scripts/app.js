a806fee41139393163030cd7e9c500134ef09533
Fabio Spada <fabio.spada93@gmail.com>
2015-10-18 14:54:46 +0200

    Course views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,9 +14,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-
-  app.test = [1,2,3,4,5,6,7,8,9,0];
-
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
